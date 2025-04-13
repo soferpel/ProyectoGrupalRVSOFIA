55fa650a6a3f026e5a6d8be7c13eb0d776994dc3
GiovannaLani <giovanna.lani@opendeusto.es>
2025-04-13 12:12:50 +0200

    sincronizar ropa

---
+++ Assets/@MyAssets/Scripts/Multijugador/Clients/MafiaControllerMP.cs
@@ -0,0 +1,199 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MafiaControllerMP : PersonControllerMP
+{
+
+    public string orderDescription;
+    public GameObject gun;
+    public GameObject player;
+    private int audioRandom;
+    private bool attacked = false;
+
+    protected override void Start()
+    {
+        audioRandom = UnityEngine.Random.Range(0, 3);
+        buyProbability = 1;
+        base.Start();
+    }
+
+    public void GenerateOrder()
+    {
+        string[] bodyParts = { "Torso", "Cabeza", "Pierna", "Brazo" };
+        string selectedPart = bodyParts[Random.Range(0, bodyParts.Length)];
+        orderDescription = selectedPart + "";
+        Debug.Log("Pedido del mafioso: " + orderDescription);
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.TryGetComponent<WeaponControllerMP>(out WeaponControllerMP weapon) && !attacked && weapon.isGrabbed.Value)
+        {
+            attacked = true;
+            Debug.Log("El mafioso no puede ser atacado.");
+            StopAllCoroutines();
+            StartCoroutine(HandleAttackSequence());
+        }
+    }
+
+    public IEnumerator HandleAttackSequence()
+    {
+        gun.SetActive(true);
+        agent.enabled = false;
+
+        if (player != null && IsServer)
+        {
+            Vector3 directionToPlayer = (Camera.main.transform.position - transform.position).normalized;
+            Vector3 flatDirection = new Vector3(directionToPlayer.x, 0, directionToPlayer.z);
+            Quaternion lookRotation = Quaternion.LookRotation(flatDirection);
+            Quaternion finalRotation = lookRotation * Quaternion.Euler(0, 80, 0);
+
+            float rotationSpeed = 5f;
+            audioSource[4].Stop();
+            if (audioRandom == 0)
+            {
+                audioSource[1].Play();
+            }
+            else if (audioRandom == 1)
+            {
+                audioSource[2].Play();
+            }
+            else
+            {
+                audioSource[3].Play();
+            }
+            if (animator != null)
+            {
+                animator.SetBool("lookAround", false);
+                animator.SetBool("walk", false);
+                animator.SetBool("attack", true);
+
+            }
+            while (Quaternion.Angle(transform.rotation, finalRotation) > 0.1f)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, Time.deltaTime * rotationSpeed);
+                yield return null;
+            }
+        }
+
+        if (animator != null)
+        {
+            animator.SetBool("lookAround", false);
+            animator.SetBool("walk", false);
+            animator.SetBool("attack", true);
+        }
+
+        yield return new WaitForSeconds(2f);
+
+
+        GameManager.isGameOver = true;
+
+        Debug.Log("Juego terminado. ¡Game Over!");
+
+    }
+
+    public string AppearanceDescription
+    {
+        get => appearanceDescription;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.LogError("La apariencia asignada es vacía o nula.");
+            }
+            else
+            {
+                appearanceDescription = value;
+                Debug.Log("Apariencia del mafioso asignada: " + appearanceDescription);
+            }
+        }
+    }
+    void OnEnable()
+    {
+        Clothing.OnMafiaAppearanceGenerated += HandleAppearanceGenerated;
+    }
+
+    void OnDisable()
+    {
+        Clothing.OnMafiaAppearanceGenerated -= HandleAppearanceGenerated;
+    }
+
+    private void HandleAppearanceGenerated(string description)
+    {
+        AppearanceDescription = description;
+    }
+
+    public string getGeneratedOrder()
+    {
+        return orderDescription;
+    }
+
+    protected override IEnumerator WaitAtBuyPoint()
+    {
+        slider.SetActive(true);
+        float elapsedTime = 0f;
+        while (elapsedTime < waitTimeBuyPoint && !served)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            elapsedTime += Time.deltaTime;
+            slider.SetSliderValue(elapsedTime, waitTimeBuyPoint);
+            yield return null;
+        }
+        Debug.Log("x Me voy");
+        buyPointController.FreePoint();
+        if (!served) StartCoroutine(HandleMafiaApproachAndAttack());
+        else StartCoroutine(MoveToFinalPoint());
+        slider.SetActive(false);
+    }
+
+    private IEnumerator HandleMafiaApproachAndAttack()
+    {
+        Transform playerTransform = Camera.main.transform;
+        if (playerTransform == null)
+        {
+            yield break;
+        }
+
+        agent.stoppingDistance = 2;
+        yield return StartCoroutine(MoveToPlayer());
+        Debug.Log("HE LLEGADO");
+        StartCoroutine(HandleAttackSequence());
+    }
+
+    protected IEnumerator MoveToPlayer()
+    {
+
+        if (agent.enabled)
+        {
+
+            agent.SetDestination(Camera.main.transform.position);
+
+            while (agent.enabled && (agent.pathPending || agent.remainingDistance > agent.stoppingDistance))
+            {
+                agent.SetDestination(Camera.main.transform.position);
+
+                animator.SetBool("lookAround", false);
+                animator.SetBool("walk", true);
+
+                if (agent.desiredVelocity.sqrMagnitude > 0.01f)
+                {
+                    Vector3 direction = -agent.desiredVelocity.normalized * personDirection;
+                    Quaternion targetRotation = Quaternion.LookRotation(direction);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 15f);
+                }
+
+                yield return null;
+            }
+
+            animator.SetBool("walk", false);
+            agent.updateRotation = true;
+
+
+        }
+    }
+
+}
---
+++ Assets/@MyAssets/Scripts/Multijugador/Clients/PersonControllerMP.cs
@@ -0,0 +1,348 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class PersonControllerMP : NetworkBehaviour
+{
+    public ClientTimerSlider slider;
+    public bool isAlive = true;
+    public bool isReported = false;
+    public float waitTime = 2f;
+    public bool inBuyPoint = false;
+    public bool served = false;
+    public bool inQueue = false;
+    public float waitTimeBuyPoint = 10f;
+
+    protected NavMeshAgent agent;
+    protected Animator animator;
+    protected ShopNavigator shopNavigator;
+    protected int pointsToVisit = 0;
+    protected int pointsVisited = 0;
+    [SerializeField]
+    protected Transform doorExitPoint;
+    protected Transform finalPoint;
+    protected Transform finalDestinationPoint;
+    protected bool isFinalMove = false;
+    protected Transform currentTarget;
+    protected Transform buyPoint;
+    protected BuyPointControllerMP buyPointController;
+    protected Coroutine waitTimeCoroutine;
+    protected float buyProbability = 0;
+    protected int personDirection = 1;
+    public AudioSource[] audioSource;
+    public string appearanceDescription;
+
+    protected virtual void Start()
+    {
+        audioSource = GetComponents<AudioSource>();
+        audioSource[4].Play();
+        slider.SetActive(false);
+        agent = GetComponent<NavMeshAgent>();
+        pointsToVisit = Random.Range(2, 4);
+        animator = GetComponent<Animator>();
+        agent.enabled = false;
+        if (!IsServer) return;
+        StartCoroutine(EnterFromDoor());
+    }
+
+    virtual protected IEnumerator EnterFromDoor()
+    {
+        Transform entryPoint = doorExitPoint;
+        Transform finalEntryPoint = finalPoint;
+
+        agent.enabled = false;
+        animator.SetBool("walk", true);
+
+        while (Vector3.Distance(transform.position, entryPoint.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, entryPoint.position, Time.deltaTime * 2f);
+            Vector3 direction = -(entryPoint.position - transform.position).normalized * personDirection;
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 15f);
+            }
+
+            yield return null;
+        }
+        shopNavigator.OpenDoor();
+        PlayDoorSound();
+
+        while (Vector3.Distance(transform.position, finalEntryPoint.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, finalEntryPoint.position, Time.deltaTime * 2f);
+            Vector3 direction = -(finalEntryPoint.position - transform.position).normalized * personDirection;
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 15f);
+            }
+
+            yield return null;
+        }
+
+
+        agent.enabled = true;
+        animator.SetBool("walk", false);
+        StartCoroutine(MoveToPoints());
+
+    }
+
+    protected IEnumerator MoveToPoints()
+    {
+        while (isAlive && agent.enabled)
+        {
+            if (isFinalMove)
+            {
+                DecideFinalAction();
+                yield break;
+            }
+
+            currentTarget = shopNavigator.GetRandomPointWithinStoreBounds();
+            pointsVisited++;
+
+            if (currentTarget != null)
+            {
+                yield return MoveToTarget(currentTarget, true);
+            }
+
+            yield return new WaitForSeconds(Random.Range(1, 4));
+
+            if (pointsVisited >= pointsToVisit)
+            {
+                isFinalMove = true;
+            }
+        }
+    }
+
+    protected void DecideFinalAction()
+    {
+        float decision = Random.value;
+
+        if (decision <= buyProbability && buyPoint != null)
+        {
+            StartCoroutine(MoveToBuyMode());
+        }
+        else
+        {
+            StartCoroutine(MoveToFinalPoint());
+        }
+    }
+
+    protected IEnumerator MoveToBuyMode()
+    {
+        if (!buyPointController.IsOccupied())
+        {
+            buyPointController.OccupyPoint(this);
+            yield return StartCoroutine(MoveToTarget(buyPointController.buyPointTransform, false));
+            StartWaitTimer();
+            inBuyPoint = true;
+
+        }
+        else
+        {
+            bool inQueue = buyPointController.PlaceInQueue(this);
+
+            if (inQueue)
+            {
+                int queuePosition = buyPointController.customerQueue.IndexOf(this);
+                if (queuePosition >= 0)
+                {
+                    Vector3 queuePositionOffset = buyPointController.buyPointTransform.position - new Vector3(2 * (queuePosition + 1), 0, 0);
+                    Transform queuePositionTransform = new GameObject("QueuePosition").transform;
+                    queuePositionTransform.position = queuePositionOffset;
+
+                    yield return StartCoroutine(MoveToTarget(queuePositionTransform, false));
+                }
+            }
+            else
+            {
+                StartCoroutine(MoveToFinalPoint());
+            }
+
+        }
+
+    }
+
+    protected IEnumerator MoveToTarget(Transform target, bool lookAround)
+    {
+
+        if (agent.enabled)
+        {
+
+            agent.SetDestination(target.position);
+
+            while (agent.enabled && (agent.pathPending || agent.remainingDistance > agent.stoppingDistance))
+            {
+                animator.SetBool("lookAround", false);
+                animator.SetBool("walk", true);
+
+                if (agent.desiredVelocity.sqrMagnitude > 0.01f)
+                {
+                    Vector3 direction = -agent.desiredVelocity.normalized * personDirection;
+                    Quaternion targetRotation = Quaternion.LookRotation(direction);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 15f);
+                }
+
+                yield return null;
+            }
+
+            animator.SetBool("walk", false);
+            if (lookAround) animator.SetBool("lookAround", true);
+            agent.updateRotation = true;
+
+            if (target.gameObject != null && target.gameObject.name == "RandomTarget" || target.gameObject.name == "QueuePosition")
+            {
+                Destroy(target.gameObject);
+            }
+        }
+    }
+
+    protected IEnumerator MoveToFinalPoint()
+    {
+        yield return StartCoroutine(MoveToTarget(finalPoint, false));
+        yield return StartCoroutine(ExitThroughDoor());
+    }
+
+    protected IEnumerator ExitThroughDoor()
+    {
+        Transform exitPoint = doorExitPoint;
+        Transform finalExitPoint = finalDestinationPoint;
+
+        agent.enabled = false;
+        shopNavigator.OpenDoor();
+        animator.SetBool("walk", true);
+        while (Vector3.Distance(transform.position, exitPoint.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, exitPoint.position, Time.deltaTime * 2f);
+            Vector3 direction = -(exitPoint.position - transform.position).normalized * personDirection;
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 15f);
+            }
+
+            yield return null;
+        }
+
+        PlayDoorSound();
+
+        if (this is ClientControllerMP client && client.isReported)
+        {
+            Debug.Log("Cadáver reportado. Fin del juego.");
+            GameManager.isGameOver = true;
+        }
+
+        while (Vector3.Distance(transform.position, finalExitPoint.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, finalExitPoint.position, Time.deltaTime * 2f);
+            Vector3 direction = -(finalExitPoint.position - transform.position).normalized * personDirection;
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 15f);
+            }
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+
+    protected void StartWaitTimer()
+    {
+        if (waitTimeCoroutine != null)
+        {
+            StopCoroutine(waitTimeCoroutine);
+        }
+        waitTimeCoroutine = StartCoroutine(WaitAtBuyPoint());
+    }
+
+    public void MoveToQueuePosition(Vector3 newPosition, bool inbuyPoint)
+    {
+        Transform queuePositionTransform = new GameObject("QueuePosition").transform;
+        queuePositionTransform.position = newPosition;
+
+        if (inbuyPoint)
+        {
+            StartCoroutine(MoveToBuyPoint());
+            Destroy(queuePositionTransform.gameObject);
+        }
+        else
+        {
+            StartCoroutine(MoveToTarget(queuePositionTransform, false));
+        }
+
+    }
+
+    protected virtual IEnumerator WaitAtBuyPoint()
+    {
+        slider.SetActive(true);
+        float elapsedTime = 0f;
+        while (elapsedTime < waitTimeBuyPoint && !served)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            elapsedTime += Time.deltaTime;
+            slider.SetSliderValue(elapsedTime, waitTimeBuyPoint);
+            yield return null;
+        }
+        Debug.Log("x Me voy");
+        buyPointController.FreePoint();
+        StartCoroutine(MoveToFinalPoint());
+        slider.SetActive(false);
+    }
+
+    protected virtual IEnumerator MoveToBuyPoint()
+    {
+        yield return StartCoroutine(MoveToTarget(buyPointController.buyPointTransform, false));
+
+        StartWaitTimer();
+        inBuyPoint = true;
+
+    }
+
+    public bool isOnBuyPoint()
+    {
+        return inBuyPoint;
+    }
+
+    private void PlayDoorSound()
+    {
+        if (audioSource[0] != null && audioSource[0].clip != null)
+        {
+            Debug.Log("Reproduciendo sonido de puerta...");
+            audioSource[0].Play();
+        }
+    }
+
+    public void SetBuyPoint(Transform buyPoint)
+    {
+        this.buyPoint = buyPoint;
+    }
+    public void SetFinalPoint(Transform finalPoint)
+    {
+        this.finalPoint = finalPoint;
+    }
+    public void SetDoorExitPoint(Transform doorExitPoint)
+    {
+        this.doorExitPoint = doorExitPoint;
+    }
+    public void SetFinalDestinationPoint(Transform finalDestinationPoint)
+    {
+        this.finalDestinationPoint = finalDestinationPoint;
+    }
+    public void SetBuyPointController(BuyPointControllerMP buyPointController)
+    {
+        this.buyPointController = buyPointController;
+    }
+    public void SetShopNavigator(ShopNavigator shopNavigator)
+    {
+        this.shopNavigator = shopNavigator;
+    }
+}
